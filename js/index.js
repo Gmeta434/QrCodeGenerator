7f42727d285536f2e4bb9fee400d51f736d24d62
Gmeta434 <winlih32121@gmail.com>
2025-05-25 21:08:49 +0700

    mm

--- js/index.js
+++ js/index.js
@@ -1,54 +1,54 @@
-const resultImg = document.getElementById('resultImg')
-const buttonGenerate = document.getElementById('Generate')
-const inputField = document.getElementById('input');
-const downloadButton = document.getElementById('downloadQr')
+const resultImg = document.getElementById("resultImg");
+const buttonGenerate = document.getElementById("Generate");
+const inputField = document.getElementById("input");
+const downloadButton = document.getElementById("downloadQr");
 
 // making function to get Qrcode from api
 async function getQr() {
     try {
-        const input = inputField.value
-        const valueIn = `https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${input}`
+        const input = inputField.value;
+        const valueIn = `https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${input}`;
         const response = await fetch(valueIn);
-        if(!response.ok) {
-            throw new Error(`error bos ${response.status}`)
+        if (!response.ok) {
+            throw new Error(`error bos ${response.status}`);
         }
         // using blob metod to make url
-        const blob = await response.blob()
-        const blobUrl = URL.createObjectURL(blob)
+        const blob = await response.blob();
+        const blobUrl = URL.createObjectURL(blob);
 
         // entring data for img element
-        resultImg.style.display = "block"
-        resultImg.src = blobUrl
+        resultImg.style.display = "block";
+        resultImg.src = blobUrl;
 
         // make system for download button and validation
-        downloadButton.style.color = "black"
-        downloadButton.href = blobUrl
-        downloadButton.download = `QrCode${Math.floor(Math.random() * 1000 + 1)}.png`
-    } 
-    catch (error){
-        throw new Error(`error bos ${error}`)
-    }
-    finally {
-        console.log('procesed done')
+        downloadButton.style.color = "black";
+        downloadButton.href = blobUrl;
+        downloadButton.download = `QrCode${Math.floor(
+            Math.random() * 1000 + 1
+        )}.png`;
+    } catch (error) {
+        throw new Error(`error bos ${error}`);
+    } finally {
+        console.log("procesed done");
     }
 }
 
 // making button validation for generate button
-buttonGenerate.addEventListener('click', (event) => {
-    event.preventDefault()
-    if(inputField.value.length + 1 > 1) {
-        getQr()
-        inputField.value = ""
+buttonGenerate.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (inputField.value.length + 1 > 1) {
+        getQr();
+        inputField.value = "";
     } else {
-        alert('Input is Emty 🗿')
+        alert("Input is Emty 🗿");
     }
-})
+});
 
 // making download validation
-downloadButton.addEventListener('click', (event) => {
+downloadButton.addEventListener("click", (event) => {
     // event.preventDefault()
-    if(!resultImg.src) {
-        alert('waiting load data')
+    if (!resultImg.src) {
+        alert("waiting load data");
     }
     return;
-})
+});
